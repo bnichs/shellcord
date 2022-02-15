c50b6b055fa3875b32fed47951cb8b1e602e1422
Ben <bnichs55@gmail.com>
2022-02-15 11:01:51 -0800

    Working MVP

--- init.sh
+++ init.sh
@@ -15,8 +15,9 @@ shuid(){
 good_history(){
   scord_id=$1
   exit_status=$2
+  cmd=$(history -1 | awk '{print $1}')
 
-  echo "$scord_id: $exit_status" >> $LOG_FILE
+  echo "$cmd: $scord_id: $exit_status" >> $LOG_FILE
 
 #  echo "yay, exit: $exit_status" >> $LOG_FILE
 #  if ((!exit_status)); then
@@ -32,26 +33,38 @@ export PROMPT_COMMAND="good_history ${PROMPT_COMMAND}"
 
 init_scord(){
   eval orig_"$(declare -f precmd)"
+  export orig_precmd
 
-  SCORD_SESSION=$(shuid)
 
   precmd(){
     # Get the last commands exit
     exit_status=$?
 
-    SCORD_CMD=$(shuid)
-    SCORD_ID="scord-$SCORD_SESSION-$SCORD_CMD"
-
     good_history "$SCORD_ID" "$exit_status"
+
+    # make a new id for the next command
+    SCORD_CMD=$(shuid)
+    export SCORD_ID="scord-$SCORD_SESSION-$SCORD_CMD"
+
     orig_precmd
-    echo "$SCORD_ID"
+#    echo "$SCORD_ID"
   }
+#  export precmd
 }
 
 
 if [ -z ${SCORD_INIT+x} ]; then
   echo "Setting up shellcord";
   export SCORD_INIT=1
+
+  SCORD_SESSION="$(shuid)"
+  export SCORD_SESSION
+
+  # Make the ids for the next command run
+  SCORD_CMD="$(shuid)"
+  export SCORD_CMD
+
+#  script
   init_scord
 else
   echo "Shellcord is already setup, not initializing"

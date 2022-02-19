447805d5cb14f5f53629294d0d224aa1b832cfee
Ben <bnichs55@gmail.com>
2022-02-18 17:32:43 -0800

    Setup ctx for proper cli library

--- shellcord/cli.py
+++ shellcord/cli.py
@@ -2,27 +2,61 @@ import logging
 import os
 import re
 from pathlib import Path
+from pprint import pprint
 
 import click
+from click import pass_context
 
 from shellcord.scord import Tag, ScordLog
-from shellcord.config import SCORD_LOG_REGEX
+from shellcord.config import SCORD_LOG_REGEX, FORCE_DEBUG, DEFAULT_LOG_LEVEL
 from shellcord.generator import RunbookGenerator
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 
 
-@click.group("cli")
-def cli():
+CLI_CONTEXT = dict(help_option_names=["-h", "--help"])
+
+
+# @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.option('--debug/--no-debug', type=bool, default=False)
+@click.option('--log-file', type=str, default=None,
+              help="The scord log file. If unset will search PWD for the most recent option")
+@click.group("cli", context_settings=CLI_CONTEXT)
+@click.pass_context
+def cli(ctx, debug, log_file: str):
     """Root cli"""
-    print('ff')
-    pass
+    ctx.ensure_object(dict)
+
+    global logger
+    if debug or FORCE_DEBUG:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(DEFAULT_LOG_LEVEL)
+
+    # logger = logging.getLogger()
+    logging.basicConfig()
+
+    log_file = log_file or os.environ.get("SCORD_LOG_FILE", None)
+    logger.debug("Getting commands from scord file %s", log_file)
+
+    if not log_file:
+        log_file = find_file()
+
+    if not log_file:
+        raise FileNotFoundError("No scord log file found. Has shellcord been run yet?")
+    scord_log = ScordLog.parse_file(log_file)
+
+    ctx.obj['scord_log'] = scord_log
+    ctx.obj['scord_log_path'] = log_file
+    # print(scord_log)
 
 
 def find_file():
     """Try and find a file that looks like an scord log"""
+    cwd = os.getcwd()
+    logger.debug("Searching cwd=%s for an scord log file", cwd)
     fils = []
-    for fil in os.listdir(os.getcwd()):
+    for fil in os.listdir(cwd):
         if re.match(SCORD_LOG_REGEX, fil):
             logger.debug("File %s matches", fil)
             fil = Path(fil)
@@ -39,17 +73,20 @@ def find_file():
 
 
 @cli.command()
-@click.option('--file', type=str, default=None, help="The scord log file. If unset will search PWD for the most recent option")
+# @click.option('--log-file', type=str, default=None, help="The scord log file. If unset will search PWD for the most recent option")
 @click.option('--out-file', type=click.File('w'), default=None, help="Where to write the runbook file")
-def generate(file, out_file):
-    if not file:
-        file = find_file()
-    scord_log = ScordLog.parse_file(file)
+@click.pass_context
+def generate(ctx, out_file):
+    # if not file:
+    #     file = find_file()
+    # scord_log = ScordLog.parse_file(file)
+    scord_log = ctx.obj['scord_log']
 
     gen = RunbookGenerator(scord_log)
 
     out_file = out_file or scord_log.out_fname()
     gen.make_markdown(out_file)
+
 
 
 @cli.group()
@@ -72,7 +109,8 @@ def tag_next(tag_str):
 
 @tag.command("previous")
 @click.argument('tag_str', type=str)
-def tag_previous(tag_str):
+@pass_context
+def tag_previous(ctx, tag_str):
     """
     Tag the last command run with this string
     """
@@ -84,9 +122,10 @@ def tag_previous(tag_str):
     t = Tag(scord_id=scord_id,
             tag_str=tag_str)
 
-    logger.debug("Saving tag value %s", t)
-    t.dump()
+    fpath = ctx.obj.get('scord_log_path')
+    logger.debug("Saving tag value %s in logfile %s", t, fpath)
+    t.dump(fname=fpath)
 
 
 if __name__ == '__main__':
-    cli()
+    cli(obj={})

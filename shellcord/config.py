200eea0af600c329e46aa199a9748c7b15fe9190
Ben <bnichs55@gmail.com>
2022-02-18 17:32:43 -0800

    Setup ctx for proper cli library

--- shellcord/config.py
+++ shellcord/config.py
@@ -1,4 +1,14 @@
+import logging
+# Markdown helpers
 TRIPLE_TICKS = "```"
+
+# Scord files
 SCORD_LOG_REGEX = r"scord-log-\w+.json"
 SCORD_ID_REGEX = r"scord-(\w+)-(\w+)"
 DEFAULT_OUT_FILE = "scord-out.md"
+
+# Set to get debug logging without setting the cli option
+FORCE_DEBUG = False
+
+
+DEFAULT_LOG_LEVEL = logging.INFO
